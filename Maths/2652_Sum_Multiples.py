a2d415db5c3d34a3dbf5acfd7f79fd50186a1feb
Priyanshu Jaiswal <jaiswalpriyanshu792@gmail.com>
2026-03-04 21:10:28 +0530

    4 March Question

--- Maths/2652_Sum_Multiples.py
+++ Maths/2652_Sum_Multiples.py
@@ -4,4 +4,4 @@ class Solution:
         for i in range(1, n+1):
             if i % 3 == 0 or i % 5 == 0 or i %7 == 0:
                 sum += i
-        return sum
+        return sum   
--- Maths/2729_Check_if_The_Number_is_Fascinating.py
+++ Maths/2729_Check_if_The_Number_is_Fascinating.py
@@ -1,15 +1,8 @@
-n = 192
-number = "123456789"
-num = str(n)+str(2*n)+str(3*n)
-
-for i in num:
-
-    if i in number:
-        number.replace(i, " ")
-        num.replace(i, " ")
-print(num)
-print(number)
-if  len(num) == 0:
-    print(True)
-else:
-    print(False)
+class Solution:
+    def isFascinating(self, n: int) -> bool:
+        number = "123456789"
+        num = str(n)+str(2*n)+str(3*n)
+        if sorted(num) == sorted(number):
+            return True
+        else:
+            return False
